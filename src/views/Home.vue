50c73e636d0015a68b7ab6e6840e8a22de10b1e4
Mark Barr <mark@hyperingenuity.com>
2019-03-12 14:12:26 -0400

    dapper-ui: initial project scaffolding from vue-cli

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Dapper Logo" src="../assets/logo.svg" width="200">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: { HelloWorld }
+}
+</script>
