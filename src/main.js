0865804b68a87c426fcde54bd591402113ed3928
Mark Barr <mark@hyperingenuity.com>
2019-03-14 17:27:25 -0400

    ui: design prototyping

--- src/main.js
+++ src/main.js
@@ -4,8 +4,12 @@ import router from './router';
 
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.min.css';
 
-Vue.use(Vuetify);
+Vue.use(Vuetify, {
+  iconfont: 'mdi',
+  theme: { primary: '#fff' }
+});
 
 Vue.config.productionTip = false;
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,22 @@
- <template>
+<template>
 <v-container fluid fill-height>
-    <v-layout align-center justify-center>
-      <v-flex xs12 sm8 md4>
-        <v-card class="elevation-12">
-          <div class="dapper-logo"><img src="../assets/logo.svg"></div>
-          <v-toolbar dark dense class="dapper-toolbar">
-            <v-toolbar-title>
-              Dapper LDAP + Radius
-            </v-toolbar-title>
-          </v-toolbar>
-          <v-card-text>
-            <v-form>
-              <v-text-field prepend-icon="mdi-account" name="dapper-login" label="Login" type="text" browser-autocomplete="new-password"></v-text-field>
-              <v-text-field prepend-icon="mdi-lock" name="dapper-password" label="Password" type="password" browser-autocomplete="new-password"></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="#005AA7" dark @click.native="login()">Login</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-flex>
-    </v-layout>
-  </v-container>
+  <v-layout align-center justify-center>
+    <v-flex xs12 sm8 md4>
+
+      <div class="dapper-logo"><img src="../assets/logo.svg"></div>
+      <div class="dapper-title">
+        Dapper LDAP + Radius
+      </div>
+      <v-form>
+        <v-text-field dark prepend-icon="mdi-account" name="dapper-username" label="Username" type="text" browser-autocomplete="new-password"></v-text-field>
+        <v-text-field dark prepend-icon="mdi-lock" name="dapper-password" label="Password" type="password" browser-autocomplete="new-password"></v-text-field>
+        <v-btn color="#fff" light @click.native="login()">Login</v-btn>
+      </v-form>
+
+    </v-flex>
+  </v-layout>
+
+</v-container>
 </template>
 
 <script>
@@ -42,16 +36,24 @@ export default {
 <style scoped>
 .dapper-logo {
     position: absolute;
-    width: 100px;
-    height: 100px;
-    top: -92px;
+    width: 64px;
+    height: 64px;
+    top: 0px;
+    left: 10px;
+    padding: 0;
+    margin: 0;
 }
 .dapper-title {
+    position: absolute;
     height: 32px;
+    top: 8px;
+    left: 80px;
     padding-left: 8px;
     padding-top: 4px;
-}
-.dapper-toolbar {
-    background-image: radial-gradient(#005AA7, #1565C0);
+    color: #fff;
+    font-size: 32px;
+    font-weight: 700;
+    font-style: italic;
+    text-transform: uppercase;
 }
 </style>
